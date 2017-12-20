f98544412b3cba468d7571865135aa3948f01cdf
hahajava <hahajava@naver.com>
2017-12-20 16:48:53 +0900

    my django app first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/MacBook15/Documents/someone/yourenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ blog/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
+class Post(models.Model):
+    author= models.ForeignKey('auth.User')
+    title= models.CharField(max_length=200)
